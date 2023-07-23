5226cc10719e9c6d734385cdbb0b7b3720dd4607
make <make@pplabs.org>
2023-07-23 10:37:48 +0800

    add client.js

--- docs/.vuepress/client.js
+++ docs/.vuepress/client.js
@@ -7,7 +7,10 @@ defineGiscusConfig({
   repo: "yangkun19921001/AudioVideoBlogDoc", //远程仓库
   repoId: "R_kgDOJ8K9Rw", //对应自己的仓库Id
   category: "Announcements",
-  categoryId: "DIC_kwDOJ8K9R84CX7pr" //对应自己的分类Id
+  categoryId: "DIC_kwDOJ8K9R84CX7pn", //对应自己的分类Id
+  mapping:"pathname",
+  inputPosition:"top",
+  lazyLoading:false,
 });
 
 export default defineClientConfig({
